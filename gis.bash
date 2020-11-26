b3b4fe19ef8a764adeb750061cff6a1d6eb40a33
pedrosantosarruda <pedro.arruda@pisomtech.com.br>
2020-11-26 00:36:40 -0300

    Improve code

--- gis.bash
+++ gis.bash
@@ -11,6 +11,6 @@ case "$@" in
   add|a) source ~/.gis/files.bash "add" "ac0ad678bac3406dad76e401790dddc7";;
   reset|r) source ~/.gis/files.bash "reset" "92fd9a5b249a4bae9e89fd8384461219";;
   rm) source ~/.gis/files.bash "rm" "4e864ba6-be5a-4a2e-92d9-c78f8142befe";;
-  diff|df) source ~/.gis/files.bash "diff" "4e864ba6-be5a-4a2e-92d9-c78f8142befe";;
-  *) echo "invalid argument";;
+  diff|df) source ~/.gis/files.bash "diff" "3ccfccd7-a702-4b4e-b024-d94ee9107476";;
+  *) echo -e "Invalid argument\nAvailable: add reset rm diff";;
 esac
--- uninstall.bash
+++ uninstall.bash
@@ -8,7 +8,7 @@
  #===============================================================================
 
 remove_tmp_files() {
-  local file_names=("ac0ad678bac3406dad76e401790dddc7" "92fd9a5b249a4bae9e89fd8384461219")
+  local file_names=("ac0ad678bac3406dad76e401790dddc7" "92fd9a5b249a4bae9e89fd8384461219" "3ccfccd7-a702-4b4e-b024-d94ee9107476" "4e864ba6-be5a-4a2e-92d9-c78f8142befe")
 
   for file_name in ${file_names[@]}; do
     local path="/tmp/$file_name"
